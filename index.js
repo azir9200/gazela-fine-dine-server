cd59baea76ba080338aadbc3f733860eb9457dbd
Azir@9200 <aziruddin83@hotmail.com>
2023-11-09 17:49:31 +0000

    cors bug fixed

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // middle ware
-app.use('/', cors());
+app.use(cors({ origin: ["http://localhost:5173", "https://gazela-fine-dine.web.app"] }))
 app.use(express.json());
 
 
